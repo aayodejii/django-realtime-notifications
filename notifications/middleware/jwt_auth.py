4fca0cf0a823ac9497a0c466460cf769c2f5beb3
Ayodeji Akenroye <e.manuel_07@yahoo.com>
2025-12-25 19:45:39 +0100

    add jwt authentication middleware for websocket connections

---
+++ notifications/middleware/jwt_auth.py
@@ -0,0 +1,37 @@
+from urllib.parse import parse_qs
+from django.contrib.auth.models import AnonymousUser
+from django.contrib.auth import get_user_model
+from channels.db import database_sync_to_async
+from rest_framework_simplejwt.tokens import AccessToken
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+
+User = get_user_model()
+
+
+@database_sync_to_async
+def get_user_from_token(token_string):
+    try:
+        access_token = AccessToken(token_string)
+        user_id = access_token.payload.get("user_id")
+        if user_id:
+            return User.objects.get(id=user_id)
+    except (InvalidToken, TokenError, User.DoesNotExist):
+        pass
+    return AnonymousUser()
+
+
+class JWTAuthMiddleware:
+    def __init__(self, app):
+        self.app = app
+
+    async def __call__(self, scope, receive, send):
+        query_string = scope.get("query_string", b"").decode()
+        query_params = parse_qs(query_string)
+        token = query_params.get("token", [None])[0]
+
+        if token:
+            scope["user"] = await get_user_from_token(token)
+        else:
+            scope["user"] = AnonymousUser()
+
+        return await self.app(scope, receive, send)
